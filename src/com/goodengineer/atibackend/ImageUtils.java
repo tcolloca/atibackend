8dbb394cece0c9810aeed067e1705305d17654c1
goodengineer <gutierrez_nacho@hotmail.com>
2016-08-23 00:52:59 -0300

    fixed crop bug

--- src/com/goodengineer/atibackend/ImageUtils.java
+++ src/com/goodengineer/atibackend/ImageUtils.java
@@ -4,9 +4,9 @@ public class ImageUtils {
 	
 	public static ImageSource crop(ImageSource image, int startX, int startY, int width, int height, ImageFactory imageFactory) {
 		ImageSource croppedImage = imageFactory.createEmpty(width, height);
-		for (int x = startX; x < width; x++) {
-			for (int y = startY; y < height; y++) {
-				croppedImage.setPixel(x, y, image.getPixel(x, y));
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				croppedImage.setPixel(x, y, image.getPixel(startX + x, startY + y));
 			}
 		}
 		croppedImage.normalize();
