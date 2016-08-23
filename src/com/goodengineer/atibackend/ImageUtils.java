19f6e5a413d5ffdbe017ffdaffdcac5f2964e7e0
goodengineer <gutierrez_nacho@hotmail.com>
2016-08-23 02:24:27 -0300

    implemented normalization

--- src/com/goodengineer/atibackend/ImageUtils.java
+++ src/com/goodengineer/atibackend/ImageUtils.java
@@ -1,8 +1,9 @@
 package com.goodengineer.atibackend;
 
 public class ImageUtils {
-	
-	public static ImageSource crop(ImageSource image, int startX, int startY, int width, int height, ImageFactory imageFactory) {
+
+	public static ImageSource crop(ImageSource image, int startX, int startY, int width, int height,
+			ImageFactory imageFactory) {
 		ImageSource croppedImage = imageFactory.createEmpty(width, height);
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
@@ -12,7 +13,7 @@ public class ImageUtils {
 		croppedImage.normalize();
 		return croppedImage;
 	}
-	
+
 	public static void copy(ImageSource from, ImageSource to) {
 		for (int x = 0; x < from.getWidth(); x++) {
 			for (int y = 0; y < from.getHeight(); y++) {
@@ -20,13 +21,30 @@ public class ImageUtils {
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * 
-	 * @param imageSource needs to have pixels between 0 and 255 (at least for this method)
+	 * @param imageSource
+	 *            needs to have pixels between 0 and 255 (at least for this
+	 *            method)
 	 */
 	public static void normalize(ImageSource imageSource) {
-//		TODO: implement normalization
+		int min = imageSource.getPixel(0, 0);
+		int max = min;
+		for (int x = 0; x < imageSource.getWidth(); x++) {
+			for (int y = 0; y < imageSource.getHeight(); y++) {
+				min = Math.min(min, imageSource.getPixel(x, y));
+				max = Math.max(max, imageSource.getPixel(x, y));
+			}
+		}
+//		only normalize if it's necessary
+		if (min >= 0 && max <= 255)
+			return;
+		for (int x = 0; x < imageSource.getWidth(); x++) {
+			for (int y = 0; y < imageSource.getHeight(); y++) {
+				int color = (int) ((255.0 * (imageSource.getPixel(x, y) - min)) / (max - min));
+				imageSource.setPixel(x, y, color);
+			}
+		}
 	}
 }
