ac5238e309a62b248a856f4d731395f438a340be
Tomas Colloca Bargallo <tomascllc94@gmail.com>
2016-08-27 18:06:27 -0300

    Adding uniform distribution, fixing existing noises adding percentage of noise, and adding salt and pepper noise.

---
+++ src/com/goodengineer/atibackend/transformation2/noise/SaltAndPepperNoiseTransformation.java
@@ -0,0 +1,36 @@
+package com.goodengineer.atibackend.transformation2.noise;
+
+import com.goodengineer.atibackend.model.Band;
+import com.goodengineer.atibackend.transformation2.Transformation;
+import com.goodengineer.atibackend.util.DistributionRandom;
+
+public class SaltAndPepperNoiseTransformation implements Transformation {
+
+    private final double percentage;
+    private final double salt;
+    private final double pepper;
+
+    public SaltAndPepperNoiseTransformation(double percentage, double salt, double pepper) {
+        this.percentage = percentage;
+        this.salt = salt;
+        this.pepper = pepper;
+    }
+
+    @Override
+    public void transform(Band band) {
+        for (int x = 0; x < band.getWidth(); x++) {
+            for (int y = 0; y < band.getHeight(); y++) {
+                if (DistributionRandom.nextUniform(0, 100) <= percentage) {
+                    double p = DistributionRandom.nextUniform(0, 1);
+                    double color = band.getRawPixel(x, y);
+                    if (p <= salt) {
+                        color = band.getValidMin();
+                    } else if (p >= pepper) {
+                        color = band.getValidMax();
+                    }
+                    band.setRawPixel(x, y, color);
+                }
+            }
+        }
+    }
+}
