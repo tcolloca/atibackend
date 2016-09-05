90982ead14fd51edfd13d07d4698765124d6cca9
Tomas Colloca Bargallo <tomascllc94@gmail.com>
2016-09-05 16:43:28 -0300

    hiPass + fixing other filters

--- src/com/goodengineer/atibackend/transformation2/filter/FilterTransformation.java
+++ src/com/goodengineer/atibackend/transformation2/filter/FilterTransformation.java
@@ -5,26 +5,41 @@ import com.goodengineer.atibackend.transformation2.Transformation;
 
 abstract class FilterTransformation implements Transformation {
 
+	final double[][] mask;
 	final int size;
 
-	FilterTransformation(int size) {
-		if (size % 2 == 0) {
+	FilterTransformation(double[][] mask) {
+		if (mask.length % 2 == 0) {
 			throw new IllegalArgumentException("Filter must be odd sized.");
 		}
-		this.size = size;
+		this.mask = mask;
+		this.size = mask.length;
 	}
 
-	void fillMatrix(Band band, int x, int y, double[][] matrix) {
-		for (int i = -(size - 1) / 2; i <= (size - 1) / 2; i++) {
-			for (int j = -(size - 1) / 2; j <= (size - 1) / 2; j++) {
-				matrix[i][j] = getMatrixValue(band, x, y, i, j);
+	@Override
+	public void transform(Band band) {
+		for (int x = 0; x < band.getWidth(); x++) {
+			for (int y = 0; y < band.getHeight(); y++) {
+				band.setRawPixel(x, y, applyMask(band, x, y));
 			}
 		}
 	}
 
+	private double applyMask(Band band, int x, int y) {
+		double count = 0;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				count += getMatrixValue(band, x, y, i, j) * mask[i][j];
+			}
+		}
+		return count;
+	}
+
 	private double getMatrixValue(Band band, int x, int y, int i, int j) {
-		int relativeX = x + i;
-		int relativeY = y + j;
+		int xOffset = - (size - 1) / 2 + i;
+		int yOffset = - (size - 1) / 2 + j;
+		int relativeX = x + xOffset;
+		int relativeY = y  + yOffset;
 		if (relativeX < 0 || relativeX >= band.getWidth() || relativeY < 0 || relativeY >= band.getHeight()) {
 			return band.getValidMin();
 		}
---
+++ src/com/goodengineer/atibackend/util/MaskFactory.java
@@ -0,0 +1,50 @@
+package com.goodengineer.atibackend.util;
+
+public class MaskFactory {
+
+    public static double[][] average(int size) {
+        double[][] mask = new double[size][size];
+        double value = 1.0 / (size * size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                mask[i][j] = value;
+            }
+        }
+        return mask;
+    }
+
+    public static double[][] gauss(int size, double sigma) {
+        double[][] mask = new double[size][size];
+        double gaussCount = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int xDist = (size - 1) / 2 - i;
+                int yDist = (size - 1) / 2 - j;
+                double exp  = Math.exp((Math.pow(xDist, 2) + Math.pow(yDist, 2)) / (-2 * Math.pow(sigma, 2)));
+                double gauss = 1 / (2 * Math.PI * Math.pow(sigma, 2)) * exp;
+                gaussCount += gauss;
+                mask[i][j] = gauss;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                mask[i][j] = mask[i][j] / gaussCount;
+            }
+        }
+        return mask;
+    }
+
+    public static double[][] hiPass(int size) {
+        double[][] mask = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == (size - 1) / 2 && j == (size - 1) / 2) {
+                    mask[i][j] = (size * size - 1);
+                } else {
+                    mask[i][j] = - 1;
+                }
+            }
+        }
+        return mask;
+    }
+}
