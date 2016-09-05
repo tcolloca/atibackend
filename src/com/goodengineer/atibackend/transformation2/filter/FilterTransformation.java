b21d5fbbd7613a5a9ddcbd8b0217fa0aecb891bb
goodengineer <gutierrez_nacho@hotmail.com>
2016-09-05 16:06:42 -0300

    filter transformation refactor

--- src/com/goodengineer/atibackend/transformation2/filter/FilterTransformation.java
+++ src/com/goodengineer/atibackend/transformation2/filter/FilterTransformation.java
@@ -5,29 +5,29 @@ import com.goodengineer.atibackend.transformation2.Transformation;
 
 abstract class FilterTransformation implements Transformation {
 
-    final int size;
+	final int size;
 
-    FilterTransformation(int size) {
-        if (size % 2 == 0) {
-            throw new IllegalArgumentException("Filter must be odd sized.");
-        }
-        this.size = size;
-    }
+	FilterTransformation(int size) {
+		if (size % 2 == 0) {
+			throw new IllegalArgumentException("Filter must be odd sized.");
+		}
+		this.size = size;
+	}
 
-    void fillMatrix(Band band, int x, int y, double[][] matrix) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = getMatrixValue(band, x, y, i, j);
-            }
-        }
-    }
+	void fillMatrix(Band band, int x, int y, double[][] matrix) {
+		for (int i = -(size - 1) / 2; i <= (size - 1) / 2; i++) {
+			for (int j = -(size - 1) / 2; j <= (size - 1) / 2; j++) {
+				matrix[i][j] = getMatrixValue(band, x, y, i, j);
+			}
+		}
+	}
 
-    private double getMatrixValue(Band band, int x, int y, int i, int j) {
-        int relativeX = x - (size - 1) / 2 + i;
-        int relativeY = x - (size - 1) / 2 + i;
-        if (relativeX < 0 || relativeX >= band.getWidth() || relativeY < 0 || relativeY >= band.getHeight()) {
-            return band.getValidMin();
-        }
-        return band.getPixel(relativeX, relativeY);
-    }
+	private double getMatrixValue(Band band, int x, int y, int i, int j) {
+		int relativeX = x + i;
+		int relativeY = y + j;
+		if (relativeX < 0 || relativeX >= band.getWidth() || relativeY < 0 || relativeY >= band.getHeight()) {
+			return band.getValidMin();
+		}
+		return band.getPixel(relativeX, relativeY);
+	}
 }
